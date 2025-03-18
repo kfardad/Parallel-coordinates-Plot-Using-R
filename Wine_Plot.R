dffc9fb939a7ce85da804fa419047a2d2dbb44f5
kfardad <keivan.fardad@gmail.com>
2025-03-18 09:06:54 -0400

    Add files via upload

--- Wine_Plot.R
+++ Wine_Plot.R
@@ -2,7 +2,8 @@ library(GGally)
 library(repr)
 
 options(repr.plot.width =20, repr.plot.height = 2)   
-ggparcoord(WINE_60data, columns=1:11, groupColumn =12, showPoints = TRUE, alphaLines =0.5)
+W60=read.csv("C://Users//Admin//Documents//R//WINE_60data.csv")
+ggparcoord(W60, columns=1:11, groupColumn =12, showPoints = TRUE, alphaLines =0.5)
         
 
 
